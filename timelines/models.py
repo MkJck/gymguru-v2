86f2772d6e2a57a04c4d554e499325435a803ea6
MikJack <galile1.g@yandex.ru>
2025-09-14 16:49:30 +0200

    Added models for Timeline and TimelineType, fixed KeyPhoto and Transition

--- timelines/models.py
+++ timelines/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import transaction
+import random
+
+User = get_user_model()
+
+class Timeline(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='timelines')
+    name = models.CharField(max_length=100)
+    # settings = models.JSONField()
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
+
+    class Meta:
+        unique_together = ['user', 'name']
+
+
+class TimelineType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
--- visuals/models.py
+++ visuals/models.py
@@ -7,46 +7,49 @@ class KeyPhoto(models.Model):
     tpos = models.PositiveIntegerField(db_index=True)
     weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, validators=[MinValueValidator(0), MaxValueValidator(500)])
 
-    s3_id = models.CharField(max_length=512, db_index=True)
+    s3_path = models.CharField(max_length=512, db_index=True)
 
     created = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"KeyPhoto {self.id} (Timeline {self.timeline_id}, pos={self.tpos})"
+
     class Meta:
         constraints = [models.UniqueConstraint(fields=['timeline', 'tpos'], name='unique_keyphoto_timeline_pos')]
         ordering = ["timeline", "tpos"]
-        db_table = "KeyPhoto"
+        db_table = "keyphoto"
 
         verbose_name = "KeyPhoto"
         verbose_name_plural = "KeyPhotos"
 
 
-        def __str__(self):
-            return f"KeyPhoto {self.id} (Timeline {self.timeline_id}, pos={self.tpos})"
 
 class Transition(models.Model):
     timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE, related_name="transitions")
     tpos = models.PositiveIntegerField(db_index=True)
 
-    from_keyphoto = models.ForeignKey("KeyPhoto", on_delete=models.SET_NULL, related_name="next_transition", null=True)
-    to_keyphoto = models.ForeignKey("KeyPhoto", on_delete=models.SET_NULL, related_name="prev_transition", null=True)
+    from_keyphoto = models.OneToOneField("KeyPhoto", on_delete=models.SET_NULL, related_name="next_transition", null=True)
+    to_keyphoto = models.OneToOneField("KeyPhoto", on_delete=models.SET_NULL, related_name="prev_transition", null=True)
 
-    s3_id = models.CharField(max_length=512, db_index=True)
+    s3_path = models.CharField(max_length=512, db_index=True)
 
     created = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
 
-    class Meta:
-        constraints = [models.UniqueConstraint(fields=['timeline', 'tpos'], name='unique_transition_timeline_pos')]
-        ordering = ["timeline", "tpos"]
-        db_table = "Transition"
-
-        verbose_name = "Transition"
-        verbose_name_plural = "Transitions"
-
-        def __str__(self):
+    def __str__(self):
             return (
                 f"Transition {self.id} "
                 f"(Timeline {self.timeline_id}, pos={self.tpos}, "
                 f"from {self.from_keyphoto_id}, to {self.to_keyphoto_id})"
             )
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['timeline', 'tpos'], name='unique_transition_timeline_pos')]
+        ordering = ["timeline", "tpos"]
+        db_table = "transition"
+
+        verbose_name = "Transition"
+        verbose_name_plural = "Transitions"
+
+        
